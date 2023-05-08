3fe1d3097f133d7098a2585cc6ea2dfa4601e901
DaviAlvesAWSD <alvesd7050@gmail.com>
2023-05-08 19:18:49 -0300

    atualização 08-05-2023-19h18

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,72 @@
 import styles from './styles.module.css';
 import { FaBeer } from 'react-icons/fa';
-import { MdFormatListBulleted } from "react-icons/md";
+import { useNavigate } from 'react-router-dom';
+import {
+  MdFormatListBulleted,
+  MdAccountCircle,
+  MdAddHome,
+  MdExitToApp,
+  MdManageAccounts,
+  MdPendingActions,
+} from 'react-icons/md';
 
 // @ts-ignore
-import MenuIcon from '../../assets/imagem/menuIcon.png';
+import logoLogin from '../../assets/imagem/logo.png';
 
-export function Home(){
-    return (
-        <div className={styles.container}>
-            <div className={styles.navbar}>
-              <MdFormatListBulleted className={styles.menuIcon} />
-            </div>
+export function Home() {
+  const navigate = useNavigate();
+
+  const goToExit = () => {
+    navigate('/');
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.navbar}></div>
+      <div className={styles.sidebar}>
+        <div className={styles.sideHeader}>
+          <img className={styles.logo} src={logoLogin} alt="" />
         </div>
-    );
+        <div className={styles.sideContent}>
+          <div className={styles.sidebarBtn}>
+            <p className={styles.sidebarBtnContent}>
+              <MdAddHome className={styles.incons} /> Home
+            </p>
+          </div>
+          <div className={styles.sidebarBtn}>
+            <p className={styles.sidebarBtnContent}>
+              <MdFormatListBulleted className={styles.incons} />
+              Lista de insumos
+            </p>
+          </div>
+          <div className={styles.sidebarBtn}>
+            <p className={styles.sidebarBtnContent}>
+              <MdPendingActions className={styles.incons} />
+              Solicitações de insumos
+            </p>
+          </div>
+          <div className={styles.sidebarBtn}>
+            <p className={styles.sidebarBtnContent}>
+              <MdManageAccounts className={styles.incons} />
+              Usuários
+            </p>
+          </div>
+          <div className={styles.sidebarBtn}>
+            <p className={styles.sidebarBtnContent}>
+              <MdAccountCircle className={styles.incons} /> Perfil
+            </p>
+          </div>
+          <div className={styles.sidebarBtn} onClick={goToExit}>
+            <p className={styles.sidebarBtnContent}>
+              <MdExitToApp className={styles.incons} />
+              Sair
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <span className={styles.textContent}> Bem vindo!</span>
+      </div>
+    </div>
+  );
 }
