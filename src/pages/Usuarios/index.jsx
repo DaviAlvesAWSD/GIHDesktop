b6a02662159542cd228d4168e158bbc2a1b71911
DaviAlvesAWSD <alvesd7050@gmail.com>
2023-06-04 11:05:14 -0300

    1 file modified

--- src/pages/Usuarios/index.jsx
+++ src/pages/Usuarios/index.jsx
@@ -1,19 +1,21 @@
 import styles from './styles.module.css';
-import { FaBeer } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
-import {
-  MdFormatListBulleted,
-  MdAccountCircle,
-  MdAddHome,
-  MdExitToApp,
-  MdManageAccounts,
-  MdPendingActions,
-} from 'react-icons/md';
+
+// @ts-ignore
+import { auth } from '../../services/FirebaseConfig';
+import { getFireStore } from 'firebase/firestore';
 
 // @ts-ignore
 import logoLogin from '../../assets/imagem/logo.png';
 
 export function Usuarios() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [users, setUsers] = useState([]);
+
+  const db = getFireStore(auth);
+
   return (
     <div className={styles.content}>
       <span className={styles.textContent}> Em construção!</span>
